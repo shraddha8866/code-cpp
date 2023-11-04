234cc00bf8dea44954e439a9d860dd1b5d31e95d
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 20:37:33 +0530

    Update array6.cpp

--- array6.cpp
+++ array6.cpp
@@ -1,41 +1,26 @@
 //Write a program to implement selection sort
-
 #include<iostream>
 using namespace std;
-void swapping(int &a, int &b) {
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
-}
-void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
-      cout << array[i] << " ";
-   cout << endl;
-}
-void selectionSort(int *array, int size) {
-   int i, j, imin;
-   for(i = 0; i<size-1; i++) {
-      imin = i;
-      for(j = i+1; j<size; j++)
-         if(array[j] < array[imin])
-            imin = j;
-         swap(array[i], array[imin]);
-   }
-}
-int main() {
-   int n;
-   cout << "Enter the number of elements: ";
-   cin >> n;
-   int arr[n]; 
-   cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
-      cin >> arr[i];
-   }
-   cout << "Array before Sorting: ";
-   display(arr, n);
-   selectionSort(arr, n);
-   cout << "Array after Sorting: ";
-   display(arr, n);
+int main(){
+    int size;
+    cout<<"Enter the size of the array";
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++){
+        cout<<"Enter the  elements";
+        cin>>arr[i];
+    }
+    for(int i=0;i<size-2;i++){
+        for(int j=i+1;j<size-1;j++){
+            if(arr[j]<arr[i]){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i];
+    }
 }
 
