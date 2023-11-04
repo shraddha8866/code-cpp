33a0f63739643f25b477ef8d25d237433e5ee701
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:11:08 +0530

    Update a3.cpp

--- a3.cpp
+++ a3.cpp
@@ -1,13 +1,16 @@
 //Write a program to swap 2 numbers without using the third variable
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
-int a=5, b=10;      
-cout<<"Before swap a= "<<a<<" b= "<<b<<endl;      
-a=a*b;
-b=a/b; 
-a=a/b;   
-cout<<"After swap a= "<<a<<" b= "<<b<<endl;      
-return 0;  
-} 
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a";
+    cin>>a;
+    cout<<"Enter b";
+    cin>>b;
+    cout<<"Before Swapping a: "<<a<<" and b: "<<b<<endl;
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    cout<<"After Swapping a: "<<a<<" and b: "<<b<<endl;
+    return 0;
+}
