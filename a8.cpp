4c1bf003b792fbe9050263149d58d85eee45038d
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:14:28 +0530

    Update a8.cpp

--- a8.cpp
+++ a8.cpp
@@ -1,37 +1,19 @@
 //Write a program to print all the prime numbers under 10000
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-bool isPrime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    if (num <= 3) {
-        return true;
-    }
-    if (num % 2 == 0 || num % 3 == 0) {
-        return false;
-    }
-    
-    for (int i = 5; i * i <= num; i += 6) {
-        if (num % i == 0 || num % (i + 2) == 0) {
-            return false;
+int main(){
+    int i,j,flag=0;
+    cout<<"Prime numebers: ";
+    for(i=2;i<=10000;i++){
+        for(j=2;j<=sqrt(i);j++){
+            if(i%j==0){
+                flag=0;
+                break;
+            }
+        }
+        if(flag==0){
+            cout<<i<<" ";
         }
     }
-    
-    return true;
-}
-
-int main() {
-    cout << "Prime numbers under 10,000:" << endl;
-    for (int i = 2; i < 10000; ++i) {
-        if (isPrime(i)) {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-
     return 0;
 }
-
