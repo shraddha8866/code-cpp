ebe9e02034c0d8890bc5eebf0f8a60156ee034a9
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:11:46 +0530

    Update a4.cpp

--- a4.cpp
+++ a4.cpp
@@ -1,26 +1,20 @@
 //Write a program to find if the number is prime or not
 #include<bits/stdc++.h>
 using namespace std;
-
-int main () 
-{
-  int n, flag = 0;
-  
-  cout << "Enter the number to check if it is prime or not: ";
-  cin >> n;
-  
-  for (int i = 2; i <= sqrt (n); i++)
-    {
-      if (n % i == 0){
-	    flag = 1;
-	    break;
-	  }
+int main(){
+    int n,flag=0;
+    cout<<"Enter number to check whether it's prime or not";
+    cin>>n;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            flag=1;
+            break;
+        }
     }
-  
-  if (flag == 0)
-    cout << "The given number is prime";
-  else 
-   cout << "The given number is not prime";
-  
-  return 0;
+    if(flag==0){
+        cout<<"Entered number is prime";
+    }
+    else{
+        cout<<"Entered number is not prime";
+    }
 }
