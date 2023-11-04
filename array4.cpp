b5cb85270be789d94406c427ff112580280e282e
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:20:44 +0530

    Update array4.cpp

--- array4.cpp
+++ array4.cpp
@@ -1,34 +1,23 @@
 //Write a program to find the middle element of an array
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main() {
+int main(){
     int size;
-
-    cout << "Enter the size of the array: ";
-    cin >> size;
-
-    if (size <= 0) {
-        cout << "Array size should be greater than 0." << endl;
-        return 1;
-    }
-
+    cout<<"Enter the size of the array :";
+    cin>>size;
     int arr[size];
-
-    cout << "Enter the elements of the array: ";
-    for (int i = 0; i < size; i++) {
-        cin >> arr[i];
+    for(int i=0;i<size;i++){
+        cout<<"Enter number "<<i+1<<" : ";
+        cin>>arr[i];
     }
-
-    if (size % 2 == 0) {
-        int middle1 = arr[size / 2 - 1];
-        int middle2 = arr[size / 2];
-        cout << "Middle elements: " << middle1 << " and " << middle2 << endl;
-    } else {
-        int middle = arr[size / 2];
-        cout << "Middle element: " << middle << endl;
+    if(size%2==0){
+        int middle_Ele1=arr[(size/2)-1];
+        int middle_Ele2=arr[size/2];
+        cout<<"Middle element of the array is "<<middle_Ele1<<" and "<<middle_Ele2;
     }
-
+    else{
+        int middle_Element=arr[size/2];
+        cout<<"Middle element of the array is "<<middle_Element;
+    }
     return 0;
 }
