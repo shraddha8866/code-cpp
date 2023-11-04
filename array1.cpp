633fd5357b31ad9a9075b315cce15cbaed708440
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:15:13 +0530

    Update array1.cpp

--- array1.cpp
+++ array1.cpp
@@ -1,29 +1,17 @@
 //Write a program to create 'n' size array entered  by the user and print the array in reverse order.
-#include <iostream>  
-using namespace std;  
-  
-int main ()  
-{  
-    int arr[50], num, temp, i, j;  
-    cout << " Please, enter the total no. you want to enter: ";  
-    cin >> num;   
-    for (i = 0; i < num; i++)  
-    {  
-        cout << " Enter the element " << i+1 << ": ";  
-        cin >> arr[i];  
-    }  
-      
-      
-    for ( i = 0, j = num - 1; i < num/2; i++, j--)  
-    {     
-        temp = arr[i];  
-        arr[i] = arr[j];  
-        arr[j] = temp;  
-    }  
-    cout << "\n Reverse all elements of the array: " << endl;  
-    for ( i = 0; i < num; i++)  
-    {  
-        cout << arr[i] << " ";  
-    }  
-    return 0;  
-} 
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout<<"Enter the size of the array";
+    cin>>size;
+    int array[size];
+    for(int i=0;i<size;i++){
+        cout<<"Enter number "<<i+1<<" :";
+        cin>>array[i];
+    }
+    for(int j=(size-1);j>=0;j--){
+        cout<<array[j];
+    }
+    return 0;
+}
