fd70df0ed4dcae9afe631e8202af23b73a36623d
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:13:06 +0530

    Update a6.cpp

--- a6.cpp
+++ a6.cpp
@@ -1,21 +1,20 @@
 //Write a program to find if the number is palindrome or not.
-#include <iostream>  
-using namespace std;  
-int main()  
-{  
-  int n,r,sum=0,temp;    
-  cout<<"Enter the Number=";    
-  cin>>n;    
- temp=n;    
- while(n>0)    
-{    
- r=n%10;    
- sum=(sum*10)+r;    
- n=n/10;    
-}    
-if(temp==sum)    
-cout<<"Number is Palindrome.";    
-else    
-cout<<"Number is not Palindrome.";   
-  return 0;  
-}  
+#include<iostream>
+using namespace std;
+int main(){
+    int rem,rev,temp,num;
+    cout<<"ENner the number to check whether it's palindrome or not";
+    cin>>num;
+    temp=num;
+    while(temp>0){
+        rem=temp%10;
+        rev=(rev*10)+rem;
+        temp=temp/10;
+    }
+    if(rev==num){
+        cout<<"Entered number is palindrome";
+    }
+    else{
+        cout<<"Entered number is not palindrome"
+    }
+}
