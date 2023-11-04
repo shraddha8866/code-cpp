4ca0557e6724ca10f18e4e984007aa77923683b5
shraddha8866 <136013810+shraddha8866@users.noreply.github.com>
2023-11-04 14:21:41 +0530

    Update array5.cpp

--- array5.cpp
+++ array5.cpp
@@ -1,55 +1,35 @@
 //Write a  program to implement binary search algorithm. Consider the input array is already sorted.
-#include <iostream>
-#include <vector>
-
+#include<iostream>
 using namespace std;
-
-int binarySearch(const vector<int>& arr, int target) {
-    int left = 0;
-    int right = arr.size() - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] == target) {
-            return mid;
+int binarysearch(int array[],int n,int key){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(array[mid]==key){
+           return mid;
         }
-
-        if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        else if(array[mid]>key){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
         }
     }
-
     return -1;
 }
-
-int main() {
-    vector<int> arr;
+int main(){
     int n;
-
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
-
-    cout << "Enter the sorted array elements: ";
-    for (int i = 0; i < n; i++) {
-        int element;
-        cin >> element;
-        arr.push_back(element);
+    cout<<"Enter the size of the array :";
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cout<<"ENter the elements :";
+        cin>>array[i];
     }
-
-    int target;
-    cout << "Enter the target element to search for: ";
-    cin >> target;
-
-    int result = binarySearch(arr, target);
-
-    if (result != -1) {
-        cout << "Element found at index " << result << endl;
-    } else {
-        cout << "Element not found in the array" << endl;
-    }
-
+    int key;
+    cout<<"Enter the key :";
+    cin>>key;
+    cout<<binarysearch(array,n,key)<<endl;;
     return 0;
 }
